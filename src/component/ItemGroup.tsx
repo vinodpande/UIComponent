626b30900f144d3243fe6b6be585b1b011673c57
Vinod <vinodkpande2014@gmail.com>
2021-10-20 12:31:54 +0530

    Style added to component

--- src/component/ItemGroup.tsx
+++ src/component/ItemGroup.tsx
@@ -1,37 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 const ItemGroup = () => {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        backgroundColor: '#000000',
-        alignItems: 'flex-start',
-        margin: 4,
-        padding: 2,
-      }}>
-      <Text
-        style={{
-          color: '#ffffff',
-          backgroundColor: 'red',
-          borderRadius: 10,
-          paddingTop: 2,
-          paddingBottom: 2,
-          paddingLeft: 8,
-          paddingRight: 8,
-        }}>
-        1
-      </Text>
-      <Text
-        style={{
-          color: '#ffffff',
-          padding: 2,
-          marginLeft: 8,
-        }}>
-        Hello From Vinod
-      </Text>
+    <View style={style.container}>
+      <Text style={style.circle}>1</Text>
+      <Text style={style.text}>Hello From Vinod</Text>
     </View>
   );
 };
 
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: '#000000',
+    alignItems: 'flex-start',
+    margin: 4,
+    padding: 2,
+  },
+  circle: {
+    color: '#ffffff',
+    backgroundColor: 'red',
+    borderRadius: 10,
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+  text: {
+    color: '#ffffff',
+    padding: 2,
+    marginLeft: 8,
+  },
+});
 export default ItemGroup;
